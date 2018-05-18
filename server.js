00d584a8529826d650a4c0559a5446bc1ae01b5c
nicholas eymann <nicholas.eymann@heliosinteractive.com>
2018-05-18 11:15:18 -0700

    this doesn't work

--- server.js
+++ server.js
@@ -1,10 +1,68 @@
-const app = require('http').createServer(handler)
-const io = require('socket.io')(app);
-const fs = require('fs');
-
-app.listen(80);
+const express = require('express');
+const http = require('http');
+const app = express();
+const router = express.Router();
 
 const SerialPort = require('serialport');
+const microbit = new SerialPort('COM4', {
+  baudRate: 115200
+});
+
+microbit.write('main screen turn on', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message);
+  }
+  console.log('microbit connected');
+});
+
+// Open errors will be emitted as an error event
+microbit.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
+//const app = require('http').createServer(handler)
+const port = 3000;
+app.set('port', port);
+app.use(express.static(`${__dirname}/dist`)); // set the static files location for the static html
+app.engine('.html', require('ejs').renderFile);
+app.set('views', `${__dirname}/dist`);
+router.get('/*', (req, res, next) => { res.sendFile(`${__dirname}/dist/index.html`); });
+app.use('/', router);
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  microbit.on('data', (data) => {
+    socket.emit('vote', { vote: `${data}` });
+  });
+});
+
+function broadcastTime() {
+  io.sockets.emit('getTime', 'wat');
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+        ? `pipe ${addr}`
+        : `port ${addr.port}`;
+
+  setInterval(broadcastTime, 1000);
+  console.log(`Listening on ${bind}`);
+}
+server.on('listening', onListening);
+
+server.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.info(`App running in ${process.env.NODE_ENV || 'none'} mode on port ${port}`);
+});
+
+module.exports = server;
+
+/* const SerialPort = require('serialport');
 const port = new SerialPort('COM4', {
   baudRate: 115200
 });
@@ -39,3 +97,4 @@ io.on('connection', function (socket) {
     socket.emit('vote', { vote: `${data}` });
   });
 });
+ */
